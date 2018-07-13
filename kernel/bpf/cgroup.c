d540e11a4302a5dba22ff6f280c4075bf19eced7
Roman Gushchin <guro@fb.com>
2018-07-13 12:41:11 -0700

    bpf: fix rcu annotations in compute_effective_progs()
    The progs local variable in compute_effective_progs() is marked
    as __rcu, which is not correct. This is a local pointer, which
    is initialized by bpf_prog_array_alloc(), which also now
    returns a generic non-rcu pointer.
    
    The real rcu-protected pointer is *array (array is a pointer
    to an RCU-protected pointer), so the assignment should be performed
    using rcu_assign_pointer().
    
    Fixes: 324bda9e6c5a ("bpf: multi program support for cgroup+bpf")
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -96,7 +96,7 @@ static int compute_effective_progs(struct cgroup *cgrp,
 				  enum bpf_attach_type type,
 				  struct bpf_prog_array __rcu **array)
 {
-	struct bpf_prog_array __rcu *progs;
+	struct bpf_prog_array *progs;
 	struct bpf_prog_list *pl;
 	struct cgroup *p = cgrp;
 	int cnt = 0;
@@ -127,7 +127,7 @@ static int compute_effective_progs(struct cgroup *cgrp,
 		}
 	} while ((p = cgroup_parent(p)));
 
-	*array = progs;
+	rcu_assign_pointer(*array, progs);
 	return 0;
 }
 
