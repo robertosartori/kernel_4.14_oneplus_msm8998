cebf01da188dac51d0bbaf41844969d5ea144697
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-12-13 22:57:16 +0100

    test2

--- drivers/gpu/drm/msm/msm_smmu.c
+++ drivers/gpu/drm/msm/msm_smmu.c
@@ -506,6 +506,8 @@ static int msm_smmu_fault_handler(struct iommu_domain *domain,
 
 	/* see iommu.h for fault flags definition */
 	SDE_EVT32(iova, flags);
+	DRM_ERROR("trigger dump, iova=0x%08lx, flags=0x%x\n", iova, flags);
+	DRM_ERROR("SMMU device:%s", client->dev ? client->dev->kobj.name : "");
 
 	/* generate dump, but no panic */
 	SDE_DBG_DUMP("all", "dbg_bus", "vbif_dbg_bus");
