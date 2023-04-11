acd75b1569bcba8aa540494dbf8f609a0e704cea
anonymous <>
2023-04-11 10:33:27 +0200

    treewide: Linux 4.14.329

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -888,7 +888,8 @@ next_step:
 
 		if (phase == 3) {
 			inode = f2fs_iget(sb, dni.ino);
-			if (IS_ERR(inode) || is_bad_inode(inode))
+			if (IS_ERR(inode) || is_bad_inode(inode) ||
+					special_file(inode->i_mode))
 				continue;
 
 			/* if inode uses special I/O path, let's go phase 3 */
