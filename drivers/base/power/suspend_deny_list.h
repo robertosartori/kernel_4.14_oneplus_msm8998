634d83a3316fa2e6ab715bd17ebec17492a5d4cc
Roberto Sartori <roberto.sartori.android@gmail.com>
2025-01-23 10:26:26 +0100

    suspend25
    suspend24 was stable for 2 weeks.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- drivers/base/power/suspend_deny_list.h
+++ drivers/base/power/suspend_deny_list.h
@@ -6,20 +6,11 @@ static const char *suspend_deny_list[] = {
 	"c8c0000.qcom,mmsscc",
 	"5065000.qcom,early_gpucc",
 	"179c8000.cprh-ctrl",
-	"regulator.70",
 	"179c4000.cprh-ctrl",
-	"regulator.71",
 	"5061000.cpr4-ctrl",
-	"regulator.72",
 	"c8ce020.qcom,gdsc",
-	"regulator.73",
 	"5065000.qcom,gpucc",
-	"179c0000.qcom,cpu-clock-8998",
-	"179c0000.qcom,cpu-clock-8998:qcom,limits-dcvs@179ce800",
-	"179c0000.qcom,cpu-clock-8998:qcom,limits-dcvs@179cc800",
 	"5066094.qcom,gdsc",
-	"regulator.74",
-	"soc:qcom,msm-cpufreq",
 };
 
 #endif // SUSPEND_DENY_LIST_H
