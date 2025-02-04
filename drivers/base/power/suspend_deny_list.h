7c19e860d3ab4cd94d82b864dfd1e02774989789
Roberto Sartori <roberto.sartori.android@gmail.com>
2025-02-05 00:25:07 +0100

    suspend27
    suspend26 failed, confirming that suspend25 *should* be working.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- drivers/base/power/suspend_deny_list.h
+++ drivers/base/power/suspend_deny_list.h
@@ -2,14 +2,10 @@
 #define SUSPEND_DENY_LIST_H
 
 static const char *suspend_deny_list[] = {
-	"100000.qcom,gcc",
-	"c8c0000.qcom,mmsscc",
-	"5065000.qcom,early_gpucc",
 	"179c8000.cprh-ctrl",
 	"179c4000.cprh-ctrl",
 	"5061000.cpr4-ctrl",
 	"c8ce020.qcom,gdsc",
-	"5065000.qcom,gpucc",
 	"5066094.qcom,gdsc",
 };
 
